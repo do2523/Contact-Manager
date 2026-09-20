355f887eb18f2c49a760d72cdeeeca3c26d9aafb
Tyler <t.w.chin05@gmail.com>
2026-09-20 18:17:43 -0400

    Updated Comments

--- database/seed.sql
+++ database/seed.sql
@@ -2,7 +2,7 @@ USE contact_manager;
 
 DELIMITER //
 
---Inserts 10 records of test contact data into contact
+--Creates procedure to insert 10 records of test contact data into contact
 CREATE PROCEDURE fill_contacts()
 BEGIN
     DECLARE i INT DEFAULT 1;
@@ -26,7 +26,7 @@ BEGIN
     END WHILE;
 END//
 
---Inserts 5 records of test user data into users
+--Creates procedure to insert 5 records of test user data into users
 CREATE PROCEDURE fill_users()
 BEGIN
     DECLARE i INT DEFAULT 1;
@@ -43,5 +43,6 @@ END//
 
 DELIMITER ;
 
+--Calls procedures to seed database
 CALL fill_users();
 CALL fill_contacts();
