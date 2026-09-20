ea0205048981163bcf6bd46737047fae46fcce1d
Tyler <t.w.chin05@gmail.com>
2026-09-20 18:11:58 -0400

    Logical fixes to insertion, and looping

--- database/seed.sql
+++ database/seed.sql
@@ -5,16 +5,22 @@ DELIMITER //
 --Inserts 10 records of test contact data into contact
 CREATE PROCEDURE fill_contacts()
 BEGIN
-    DECLARE i INT DEFAULT 0;
+    DECLARE i INT DEFAULT 1;
+    DECLARE J INT DEFAULT 1;
 
-    WHILE i < 10 DO
-        INSERT INTO CONTACTS(FirstName, LastName, Email, Phone, DateCreated)
-        VALUES (CONCAT('John_' , i),
+    WHILE i <= 10 DO
+        INSERT INTO contacts(UserID, FirstName, LastName, Email, Phone, DateCreated)
+        VALUES (j,
+                CONCAT('John_' , i),
                 CONCAT('Doe_' , i),
                 CONCAT('TestEmail_' , i),
-                REPEAT(i,10),
+                REPEAT(CONCAT(i),10),
                 NOW()
-        );    
+        );
+
+        IF (i % 2) = 0 THEN
+            SET j = j + 1;
+        END IF;
 
         SET i = i + 1;
     END WHILE;
@@ -23,10 +29,10 @@ END//
 --Inserts 5 records of test user data into users
 CREATE PROCEDURE fill_users()
 BEGIN
-    DECLARE i INT DEFAULT 0;
+    DECLARE i INT DEFAULT 1;
 
-    WHILE i < 5 DO
-        INSERT INTO USERS(Username,Password)
+    WHILE i <= 5 DO
+        INSERT INTO users(Username, Password)
         VALUES (CONCAT('TestUser_' , i),
                 CONCAT('TestPass_', i)
         );
@@ -36,3 +42,6 @@ BEGIN
 END//
 
 DELIMITER ;
+
+CALL fill_users();
+CALL fill_contacts();
