47804ad1bce66acdb76b04261b36c366c7c02878
Tyler <t.w.chin05@gmail.com>
2026-09-21 20:11:59 -0400

    Beginning of script

--- database/setup.sh
+++ database/setup.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+USER="DB_User"
+PASWD="DB_Password"
+
+if ! command -v mysql > /dev/null 2>&1; then
+    echo "MySQL is not installed. Installing"
+    sudo apt update
+    sudo apt install -y mysql-server
+fi
+    echo "Setting up Database"
+    sudo mysql -u "$USER" -p"$PASWD" < schema.sql
+    sudo mysql -u "$USER" -p"$PASWD" < seed.sql
+    echo "Finished"
+    
