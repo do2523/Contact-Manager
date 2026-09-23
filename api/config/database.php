cad8fc805eed7256ee558e3d2009d06c1408bb71
Karmaxenn <jarezc605@gmail.com>
2026-09-23 15:07:36 -0400

    database

--- api/config/database.php
+++ api/config/database.php
@@ -1,14 +1,18 @@
 <?php
 
 $host = "localhost";
-$username = "";
+$username = "root";
 $password = "";
-$database = "";
+$database = "login_db";
 
 // Create MySQL connection
-
+$mysql = new mysqli(hostname: $host, username: $username, password: $password, database: $database);
 
 // Check if connection failed
+if($mysql -> connect_error)
+    {
+        die("Connection error:" . $mysql -> connect_error);
+    }
 
-
+return $mysql;
 ?>
