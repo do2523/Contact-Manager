d12c642cc738e73fdc85083e474819014e23c87f
Tyler <t.w.chin05@gmail.com>
2026-09-24 13:27:12 -0400

    Moved database and user creation to setup script

--- database/setup.sh
+++ database/setup.sh
@@ -9,8 +9,16 @@ if ! command -v mysql > /dev/null 2>&1; then
     sudo apt install -y mysql-server
 fi
     echo "Setting up Database"
-    sudo mysql -u "$USER" -p"$PASWD" < schema.sql
-    sudo mysql -u "$USER" -p"$PASWD" < seed.sql
-    sudo mysql -u "$USER" -p"$PASWD" < queries.sql
-    echo "Finished"
+    echo "Creating DB_User"
+    sudo mysql <<EOF
+    CREATE DATABASE IF NOT EXISTS contact_manger;
+    USE contact_manger; 
+    CREATE USER 'DB_User'@'%' IDENTIFIED BY 'DB_Password';
+    GRANT ALL PRIVILEGES on contact_manager. * to 'DB_User'@'%';
+    FLUSH PRIVILEGES;
+EOF 
+    mysql -u "$USER" -p"$PASWD" < schema.sql
+    mysql -u "$USER" -p"$PASWD" < seed.sql
+    mysql -u "$USER" -p"$PASWD" < queries.sql
+    echo "Finished Setting up Database"
     
