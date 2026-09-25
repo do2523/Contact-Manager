da2772b27abf5a2b298f9da9962a402a4a946ced
Tyler <t.w.chin05@gmail.com>
2026-09-25 14:02:54 -0400

    Fixed EOF spacing

--- database/setup.sh
+++ database/setup.sh
@@ -11,12 +11,13 @@ fi
     echo "Setting up Database"
     echo "Creating DB_User"
     sudo mysql <<EOF
-    CREATE DATABASE IF NOT EXISTS contact_manager;
-    USE contact_manager; 
-    CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$PASWD';
-    GRANT ALL PRIVILEGES on contact_manager.* to '$USER'@'%';
-    FLUSH PRIVILEGES;
-EOF 
+CREATE DATABASE IF NOT EXISTS contact_manager;
+USE contact_manager; 
+CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$PASWD';
+GRANT ALL PRIVILEGES on contact_manager.* to '$USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
     mysql -u "$USER" -p"$PASWD" < schema.sql
     mysql -u "$USER" -p"$PASWD" < seed.sql
     mysql -u "$USER" -p"$PASWD" < queries.sql
